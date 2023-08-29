37e96f2415dcbb0a1159f73d9126943d0aa39128
Faelayis <48393914+Faelayis@users.noreply.github.com>
2023-08-29 10:25:56 +0700

    feat: calculator

---
+++ COM 1308/29-8-66/calculator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main()
+{
+    int y;
+    int x;
+
+    printf("Enter number: ");
+    scanf("%d", &y);
+    printf("Enter another number: ");
+    scanf("%d", &x);
+    printf("You entered %d and %d result = %d", y, x, y + x);
+    return 0;
+}
