30dd7b970b8a55977ad39988677371feb854b1eb
Faelayis <48393914+Faelayis@users.noreply.github.com>
2023-09-12 10:40:12 +0700

    fix: nahh

--- COM-1308/12-09-66/more-less.c
+++ COM-1308/12-09-66/more-less.c
@@ -2,7 +2,7 @@
 
 main(int argc, char const *argv[])
 {
-   int i, x[2];
+   int i, x[2], r;
 
    for (i = 0; i < 3; i++)
    {
@@ -10,7 +10,9 @@ main(int argc, char const *argv[])
       scanf("%i", &x[i]);
    }
 
-   (x[0] + x[1] + x[2]) > 50 ? printf("More 100") : (void)NULL;
+   r = x[0] + x[1] + x[2];
+
+   (r) <= 100 ? printf("%d + %d + %d = %d", x[0], x[1], x[2], r) : printf("%d + %d + %d = %d", x[0], x[1], x[2], r + 50);
 
    printf("\n");
    return 0;
