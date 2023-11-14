8409d4aae47a0247ba04d60c58501e6128f45744
Faelayis <48393914+Faelayis@users.noreply.github.com>
2023-11-14 10:33:16 +0700

    feat: init chapter 1

---
+++ COM-1305/chapter/1/1.py
@@ -0,0 +1,9 @@
+# 1.ให้เขียนโปรแกรมแสดง รหัสนักศึกษา ชื่อ-นามสกุล และจังหวัดที่อาศัยอยู่
+
+student_id = "66143XXX"
+full_name = "Pichwach Jantharangsee"
+province = "Chiang Mai"
+
+print("รหัสนักศึกษา:", student_id)
+print("ชื่อ-นามสกุล:", full_name)
+print("จังหวัดที่อาศัย:", province)
