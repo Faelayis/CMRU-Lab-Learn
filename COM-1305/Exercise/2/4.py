e289659173f6c4592874fe1b5010b684186f3f49
Faelayis <48393914+Faelayis@users.noreply.github.com>
2023-11-21 11:35:26 +0700

    feat: update content

---
+++ COM-1305/Exercise/2/4.py
@@ -0,0 +1,14 @@
+# 1
+N1 = int(input("Enter Number 1 :"))
+N2 = int(input("Enter Number 2 :"))
+sum = N1 + N2
+
+print("Sum :", sum)
+
+# 2
+input2 = int(input("Enter Number :"))
+print(2 * input2 + 1)
+
+# 3
+input3 = int(input("Enter Number :"))
+print(2 * input3 + 1)
