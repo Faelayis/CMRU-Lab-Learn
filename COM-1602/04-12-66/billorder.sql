016281b1d02287ce36d3427092f6295cc83cc9ef
Faelayis <48393914+Faelayis@users.noreply.github.com>
2023-12-06 16:35:53 +0700

    feat: update content

---
+++ COM-1602/04-12-66/billorder.sql
@@ -0,0 +1,16 @@
+-- Create Billorder Table
+CREATE TABLE Billorder_461 (
+   IdBillorder VARCHAR(10) PRIMARY KEY,
+   Id_Sup VARCHAR(10)
+) ENGINE = MYISAM;
+
+-- Add Billorder Data
+INSERT INTO Billorder_461 (IdBillorder, Id_Sup)
+VALUES ('BC001', 'SP001'),
+   ('BC002', 'SP002'),
+   ('BC003', 'SP003'),
+   ('BC004', 'SP004');
+
+-- Show Billorder data
+SELECT *
+FROM Billorder_461;
