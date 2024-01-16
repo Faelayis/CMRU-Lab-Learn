a3a74ab8af69e62b60e4e967e590b6c8a6b9dc97
Faelayis <48393914+Faelayis@users.noreply.github.com>
2024-01-16 08:48:00 +0700

    feat: update content

--- COM-1305/Exam/test.py
+++ COM-1305/Exam/test.py
@@ -1,5 +1,14 @@
 text = input("ป้อนสัญลักษณ์พิเศษ : ")
 num = int(input("ป้อนตัวเลข : "))
 
-for i in range(1, num + 1, 2):
-    print(f"{text}{i}{text}{i+1}")
+loop = 1
+count = 0
+
+for i in range(1, num + 1):
+    count += 1
+    for j in range(1, num + 1):
+        if loop % 2 == 0:
+            print(count, end=" ")
+        else:
+            print(text, end=" ")
+        loop += 1
