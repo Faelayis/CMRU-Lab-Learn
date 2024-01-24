18abf5fbefd6e47424cc3df62c4bc6e4de8da653
Faelayis <48393914+Faelayis@users.noreply.github.com>
2024-01-24 21:26:17 +0700

    feat: support multiple image extensions
    GitHub README files support

--- script/markdown/image.ts
+++ script/markdown/image.ts
@@ -3,32 +3,40 @@ import { basename, dirname, extname, join } from "node:path";
 
 import type { Entry } from "fast-glob";
 
-export async function ImageHeader(markdownContent: string[], folder: string): Promise<string> {
-	const imagePath = `${folder}/README.png`;
+const imageExtensions = [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".svg", ".webp"];
 
+export async function ImageHeader(markdownContent: string[], folder: string): Promise<string> {
 	let readmeContent = markdownContent.join("");
 
-	try {
-		await fs.access(imagePath);
-		readmeContent = `![Preview Image](${imagePath.replace(folder, ".")})\n\n` + readmeContent;
-	} catch {
-		undefined;
+	for (const extension of imageExtensions) {
+		const imagePath = `${folder}/README${extension}`;
+
+		try {
+			await fs.access(imagePath);
+			readmeContent = `![Preview Image](${imagePath.replace(folder, ".")})\n\n` + readmeContent;
+			break;
+		} catch {
+			undefined;
+		}
 	}
 
 	return readmeContent;
 }
 
 export async function ImagePreview(file: Entry, fileSystem: typeof fs) {
-	const imageName = basename(file.path, extname(file.path)) + ".png",
-		imagePath = join(dirname(file.path), imageName);
-
 	let imagePreview = "";
 
-	try {
-		await fileSystem.access(imagePath);
-		imagePreview = `\n![${file.name}](./${imageName})\n`;
-	} catch {
-		undefined;
+	for (const extension of imageExtensions) {
+		const imageName = basename(file.path, extname(file.path)) + extension,
+			imagePath = join(dirname(file.path), imageName);
+
+		try {
+			await fileSystem.access(imagePath);
+			imagePreview = `\n![${file.name}](./${imageName})\n`;
+			break;
+		} catch {
+			undefined;
+		}
 	}
 
 	return imagePreview;
