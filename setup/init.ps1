995e44871a11843a7617379bbeb847866bd250bc
Faelayis <48393914+Faelayis@users.noreply.github.com>
2024-02-06 09:03:22 +0700

    feat: update

--- setup/init.ps1
+++ setup/init.ps1
@@ -1,3 +1,6 @@
+# Change the Execution Policy
+Set-ExecutionPolicy -Scope CurrentUser RemoteSigned
+
 # Set Personalize
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize /v AppsUseLightTheme /t REG_DWORD /d 0 /f
 reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize /v SystemUsesLightTheme /t REG_DWORD /d 0 /f
@@ -10,5 +13,5 @@ reg add "HKCU\Keyboard Layout\Toggle" /v "Language Hotkey" /t REG_SZ /d "1" /f
 # Enable "Let me use a different input method for each app window"
 Set-WinLanguageBarOption -UseLegacySwitchMode
 
-# Change the Execution Policy
-Set-ExecutionPolicy RemoteSigned
+# Restart Windows Explorer
+Stop-Process -Name explorer -Force
