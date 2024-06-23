cc8c4f7c899935068c6cd7a02ae49144f6719f40
Faelayis <48393914+Faelayis@users.noreply.github.com>
2024-06-23 13:12:07 +0700

    fix: handle potential undefined values in sortFiles function

--- script/utils/sort.ts
+++ script/utils/sort.ts
@@ -1,23 +1,28 @@
 import { Entry } from "fast-glob";
 
 export function sortFiles(files: Entry[]): Entry[] {
-	files.sort((a, b) => {
-		const aBase = a.path.split("/").pop()?.split(".").slice(0, -1).join("."),
-			bBase = b.path.split("/").pop()?.split(".").slice(0, -1).join("."),
-			aParts = aBase?.split(".") || [],
-			bParts = bBase?.split(".") || [],
-			aNumber = Number(aParts[0]),
-			bNumber = Number(bParts[0]);
+	const collator = new Intl.Collator(undefined, { numeric: true, sensitivity: "base", usage: "sort" });
+	const sortedFiles = [...files];
+
+	sortedFiles.sort((a, b) => {
+		const aBase = a.path.split("/").pop()?.split(".").slice(0, -1).join(".") ?? "";
+		const bBase = b.path.split("/").pop()?.split(".").slice(0, -1).join(".") ?? "";
+
+		const aParts = aBase.split(".");
+		const bParts = bBase.split(".");
+
+		const aNumber = Number(aParts[0]);
+		const bNumber = Number(bParts[0]);
 
 		if (aNumber !== bNumber) {
 			return aNumber - bNumber;
 		}
 
-		const aDecimal = aParts[1] || "",
-			bDecimal = bParts[1] || "";
+		const aDecimal = aParts[1] || "";
+		const bDecimal = bParts[1] || "";
 
-		return aDecimal.localeCompare(bDecimal);
+		return collator.compare(aDecimal, bDecimal);
 	});
 
-	return files;
+	return sortedFiles;
 }
